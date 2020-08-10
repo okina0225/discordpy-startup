bf44d6253166793ff10a951cc31e63b4009a1671
okina0225 <69472206+okina0225@users.noreply.github.com>
2020-08-11 01:41:32 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -11,22 +11,6 @@ async def on_command_error(ctx, error):
     orig_error = getattr(error, "original", error)
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
-
-@bot.command()
-async def add(ctx, a: int, b: int):
-    await ctx.send(a+b)
-
-@bot.command()
-async def multiply(ctx, a: int, b: int):
-    await ctx.send(a*b)
-
-@bot.command()
-async def greet(ctx):
-    await ctx.send(":smiley: :wave: Hello, there!")
-
-@bot.cmmands()
-async def cat(ctx):
-    await ctx.send("https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif")
     
 @bot.command()
 async def ping(ctx):
