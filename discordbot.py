620df0c0598f622b0f04bf977b92b33eff8cc651
okina0225 <69472206+okina0225@users.noreply.github.com>
2020-08-11 17:24:46 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,12 +1,9 @@
 from discord.ext import commands
 import os
 import traceback
-import random
-import asyncio
 
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
-client = discord.Client()
 
 
 @bot.event
